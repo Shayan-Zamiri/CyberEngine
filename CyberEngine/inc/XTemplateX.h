f393568be112bb84c6fe956468c46a21e3a6053c
Shayan <shayan.zamiri@gmail.com>
2022-03-14 14:01:27 +0330

    Added XTemplateX.h/.cpp
    - It makes it easier to create new files and classes by copying and pasting

---
+++ CyberEngine/inc/XTemplateX.h
@@ -0,0 +1,23 @@
+/* Copyright (C) 2022 Shayan Zamiri <shayan.zamiri@gmail.com> - All Rights Reserved
+ * You may use, distribute and modify this code under the terms of the MIT license.
+ * You should have received a copy of the MIT license with this file. */
+
+#pragma once
+
+namespace CE
+{
+	class XTEMPLATEX
+	{
+		// CTOR/DTOR & VIRTUAL FUNCTIONS
+	public:
+
+		// FUNCTIONS
+	private:
+
+		// GETTERS & SETTERS
+	public:
+
+		// PROPERTIES
+	private:
+	};
+}
---
+++ CyberEngine/src/XTemplateX.cpp
@@ -0,0 +1,11 @@
+/* Copyright (C) 2022 Shayan Zamiri <shayan.zamiri@gmail.com> - All Rights Reserved
+ * You may use, distribute and modify this code under the terms of the MIT license.
+ * You should have received a copy of the MIT license with this file. */
+
+#include "CEPCH.h"
+#include "XTemplateX.h"
+
+namespace CE
+{
+	
+}
