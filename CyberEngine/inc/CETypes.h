abdbfe583b849f36d03589f8b0134ebbf3a2bcfc
Shayan <shayan.zamiri@gmail.com>
2022-03-14 14:03:48 +0330

    Moved Terathon's types to CETypes

--- CyberEngine/inc/CETypes.h
+++ CyberEngine/inc/CETypes.h
@@ -1,38 +1,45 @@
 #pragma once
 
 #include <cstdint>
+#include "TSMatrix4D.h"
 
+using U8 = uint8_t;
+using U16 = uint16_t;
+using U32 = uint32_t;
+using U64 = uint64_t;
 
-using U8    = uint8_t;
-using U16   = uint16_t;
-using U32   = uint32_t;
-using U64   = uint64_t;
-
-using U8_S  = uint_least8_t;
+using U8_S = uint_least8_t;
 using U16_S = uint_least16_t;
 using U32_S = uint_least32_t;
 using U64_S = uint_least64_t;
 
-using U8_F  = uint_fast8_t;
+using U8_F = uint_fast8_t;
 using U16_F = uint_fast16_t;
 using U32_F = uint_fast32_t;
 using U64_F = uint_fast64_t;
 
-using I8    = int8_t;
-using I16   = int16_t;
-using I32   = int32_t;
-using I64   = int64_t;
+using I8 = int8_t;
+using I16 = int16_t;
+using I32 = int32_t;
+using I64 = int64_t;
 
-using I8_F  = int_fast8_t;
+using I8_F = int_fast8_t;
 using I16_F = int_fast16_t;
 using I32_F = int_fast32_t;
 using I64_F = int_fast64_t;
 
-using I8_S  = int_least8_t;
+using I8_S = int_least8_t;
 using I16_S = int_least16_t;
 using I32_S = int_least32_t;
 using I64_S = int_least64_t;
 
-using IMAX  = intmax_t;
-using UMAX  = uintmax_t;
+using IMAX = intmax_t;
+using UMAX = uintmax_t;
 
+using Matrix4D = Terathon::Matrix4D;
+using Matrix3D = Terathon::Matrix3D;
+using Matrix2D = Terathon::Matrix2D;
+using Vector4D = Terathon::Vector4D;
+using Vector3D = Terathon::Vector3D;
+using Vector2D = Terathon::Vector2D;
+using Transform4D = Terathon::Transform4D;
